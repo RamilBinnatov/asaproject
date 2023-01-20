ae8cb4aff67551764e9521b4501bf9abd69d8d8a
RamilBinnatov <ramilrb@code.edu.az>
2023-01-20 17:18:38 +0400

    asaproject

---
+++ RedParts/Final-Project/wwwroot/Assets/js/main.js
@@ -0,0 +1,94 @@
+
+let filterHead=document.querySelectorAll('.filter-box .box-head')
+let filterUp=document.querySelectorAll('.up-ico')
+let filterList=document.querySelectorAll('.box-main')
+
+for(let i=0;i<filterUp.length;i++){
+    filterHead[i].onclick=()=>{
+        filterUp[i].classList.toggle('active')
+        filterList[i].classList.toggle('active')
+    } 
+}
+
+let choose=document.querySelectorAll('.choose-li')
+let chooseMain=document.querySelectorAll('.down-choose')
+
+choose[0].onclick=function(){
+    chooseMain[0].classList.remove('active')
+    chooseMain[1].classList.remove('active')
+    chooseMain[2].classList.remove('active')
+    choose[0].classList.add('active')
+    choose[1].classList.remove('active')
+    choose[2].classList.remove('active')
+}
+choose[1].onclick=function(){
+    chooseMain[0].classList.add('active')
+    chooseMain[1].classList.add('active')
+    chooseMain[2].classList.remove('active')
+    choose[0].classList.remove('active')
+    choose[1].classList.add('active')
+    choose[2].classList.remove('active')
+}
+
+choose[2].onclick=function(){
+    chooseMain[0].classList.add('active')
+    chooseMain[2].classList.add('active')
+    chooseMain[1].classList.remove('active')
+    choose[0].classList.remove('active')
+    choose[1].classList.remove('active')
+    choose[2].classList.add('active')
+}
+
+
+
+let labelProduct=document.querySelectorAll('.product-label')
+
+for(let i=0;i<labelProduct.length;i++){
+    labelProduct[i].onclick=()=>{
+        removeActive()
+        labelProduct[i].classList.add('active')
+    }
+}
+
+const removeActive = () => {
+   
+    for(let i=0;i<labelProduct.length;i++){
+        labelProduct[i].classList.remove('active')
+    }
+ }
+ 
+
+
+ let colorProduct=document.querySelectorAll('.color-box')
+
+for(let i=0;i<colorProduct.length;i++){
+    colorProduct[i].onclick=()=>{
+        removecolorActive()
+        colorProduct[i].classList.add('active')
+    }
+}
+
+const removecolorActive = () => {
+   
+    for(let i=0;i<colorProduct.length;i++){
+        colorProduct[i].classList.remove('active')
+    }
+ }
+ 
+
+ 
+let minus=document.querySelector('.minus')
+ let plus=document.querySelector('.plus')
+ let count=document.querySelector('.count')
+
+ minus.onclick=function(){
+   
+    if(count.innerText>1){
+     count.innerText=Number(count.innerText) -1;
+   }
+ }
+ plus.onclick=function(){
+    count.innerText=Number(count.innerText) +1;
+ }
+
+ 
